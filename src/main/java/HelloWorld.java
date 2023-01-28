575b890c760956b8e06add74c33984d43ac2b3cf
469228402@qq.com <yaotingli>
2023-01-28 11:57:29 +0800

    添加注释

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,3 +1,9 @@
+/**
+ * Demo类
+ *
+ * @author MrLee
+ * @date 2023/1/28 11:50
+ */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World.");
